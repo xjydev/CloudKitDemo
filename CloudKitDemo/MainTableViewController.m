afe723ede025a7d8d349e8785499c6cec639e10d
jingyuan5 <jingyuan5@staff.weibo.com>
2023-06-11 21:41:27 +0800

    1

--- CloudKitDemo/MainTableViewController.m
+++ CloudKitDemo/MainTableViewController.m
@@ -7,6 +7,48 @@
 
 #import "MainTableViewController.h"
 
+@interface ActivityIndView : UIView
+
+@end
+@implementation ActivityIndView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if(self){
+        CFTimeInterval duration = 1;
+        CFTimeInterval beginTime = CACurrentMediaTime();
+        NSArray *beginTimes = @[@(0.4),@(0.2),@(0.0),@(0.2),@(0.4)];
+        CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.85 :0.25 :0.37 :0.85];
+        CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale.y"];
+        animation.keyTimes = @[@(0.0),@(0.5),@(1)];
+        animation.timingFunctions = @[timingFunction,timingFunction];
+        animation.values =@[@(1),@(0.4),@(1)];
+        animation.duration = duration;
+        animation.repeatCount = HUGE;
+        animation.removedOnCompletion = NO;
+        CGFloat width = frame.size.width/9;
+        for (NSInteger i = 0;i<5;i++) {
+            CAShapeLayer *line = [CAShapeLayer layer];
+            CGRect rect = CGRectMake((width*2)*i, 0,width, frame.size.height);
+            UIBezierPath *path =[UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, width, frame.size.height) cornerRadius:width/2.0];
+//            [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 10, 60)];
+            line.path = path.CGPath;
+            line.lineJoin = kCALineJoinRound;
+            line.lineCap = kCALineCapRound;
+            line.fillColor = [UIColor redColor].CGColor;
+            line.frame = rect;
+            animation.beginTime = beginTime+[beginTimes[i] doubleValue];
+            [line addAnimation:animation forKey:@"animation"];
+            [self.layer addSublayer:line];
+            
+        }
+        
+    }
+    return self;
+}
+
+@end
+
 @interface MainTableViewController ()
 @property (nonatomic, strong)NSArray *array;
 @end
@@ -22,22 +64,26 @@
     
     
     UIView *headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
-    headerView.backgroundColor = [UIColor redColor];
+    headerView.backgroundColor = [UIColor yellowColor];
     self.tableView.tableHeaderView = headerView;
     
     
-    UIView *cView = [[UIView alloc]initWithFrame:CGRectMake(50, 50, 54, 14)];
-    cView.backgroundColor = [UIColor greenColor];
-    CGAffineTransform transform = CGAffineTransformIdentity;
-    transform.c = -0.2;
-    transform.b = 0;
-    CALayer *layer = [CALayer layer];
-    layer.frame = CGRectMake(3, 0, cView.frame.size.width - 6, cView.frame.size.height);
-    layer.cornerRadius = 3;
-    layer.backgroundColor = [UIColor redColor].CGColor;
-    layer.affineTransform = transform;
-    cView.layer.mask = layer;
-    [headerView addSubview:cView];
+//    UIView *cView = [[UIView alloc]initWithFrame:CGRectMake(50, 50, 54, 14)];
+//    cView.backgroundColor = [UIColor greenColor];
+//    CGAffineTransform transform = CGAffineTransformIdentity;
+//    transform.c = -0.2;
+//    transform.b = 0;
+//    CALayer *layer = [CALayer layer];
+//    layer.frame = CGRectMake(3, 0, cView.frame.size.width - 6, cView.frame.size.height);
+//    layer.cornerRadius = 3;
+//    layer.backgroundColor = [UIColor redColor].CGColor;
+//    layer.affineTransform = transform;
+//    cView.layer.mask = layer;
+//    [headerView addSubview:cView];
+    
+    ActivityIndView *aview = [[ActivityIndView alloc]initWithFrame:CGRectMake(50, 50, 50, 50)];
+    aview.backgroundColor =[UIColor grayColor];
+    [headerView addSubview:aview];
     
 }
 
@@ -62,6 +108,9 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     UIViewController *VC = segue.destinationViewController;
     VC.title = sender;
+}
+-(BOOL)prefersStatusBarHidden {
+    return NO;
 }
 /*
 // Override to support conditional editing of the table view.
